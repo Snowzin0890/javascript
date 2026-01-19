8aabdba8350407d0466f9b64e7f02e52cd7831f6
Tommaso <valverdetommaso@gmail.com>
2026-01-19 20:45:39 -0300

    att

---
+++ aula14/ambiente.js
@@ -0,0 +1,11 @@
+for(i = 0; i <= 100; i++){
+    if(i <= 10){
+        console.log(`Voce tem ${i} anos. Voce e crianca.`)
+    }else if(i <= 21){
+        console.log(`Voce tem ${i} anos. Voce e jovem.`)
+    }else if(i <= 50){
+        console.log(`Voce tem ${i} anos. Voce e adulto.`)
+    }else{
+        console.log(`Voce tem ${i} anos. Voce e idoso.`)
+    }
+}
